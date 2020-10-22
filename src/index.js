57ec27f3251e274ce84b3020733fee1c17e2198f
Maxim Zabolotny <maxim.zabolotny20121776@gmail.com>
2020-10-22 11:44:54 +0300

    feat: add solutions for task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let chars = str.split(''),
+      newBrackets = [],
+      stack = [];
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+      newBrackets[bracketsConfig[i][0]] = bracketsConfig[i][1];
+
+  }
+
+  for (let i = 0; i < chars.length; i++) {
+      if (chars[i] === stack[stack.length - 1]) {
+          stack.pop();
+      } else {
+          if (newBrackets[chars[i]] !== undefined) stack.push(newBrackets[chars[i]]);
+          else return false;
+      }
+  }
+
+  return stack.length == 0;
 }
